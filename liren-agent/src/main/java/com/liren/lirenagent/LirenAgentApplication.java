77c42d1915368e3efc0da8e05fdeae24fb5c2b7e
lirendada <2916776007@qq.com>
2026-03-11 22:01:18 +0800

    搭建后端框架

---
+++ liren-agent/src/main/java/com/liren/lirenagent/LirenAgentApplication.java
@@ -0,0 +1,13 @@
+package com.liren.lirenagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LirenAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LirenAgentApplication.class, args);
+    }
+
+}
