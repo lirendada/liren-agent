f5c39d48ad2f3dfab809eed13916f83578f079c3
lirendada <2916776007@qq.com>
2026-03-14 15:58:07 +0800

    学习http方式接入api

---
+++ liren-agent/src/main/java/com/liren/lirenagent/demo/invoke/HttpAiInvoke.java
@@ -0,0 +1,48 @@
+package com.liren.lirenagent.demo.invoke;
+
+import cn.hutool.http.HttpRequest;
+import cn.hutool.http.HttpResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class HttpAiInvoke {
+
+    @Value("${dashscope.api-key}")
+    private String apiKey;
+
+    public void run() {
+        String url = "https://dashscope.aliyuncs.com/api/v1/services/aigc/text-generation/generation";
+
+        String jsonBody = """
+        {
+            "model": "qwen-plus",
+            "input":{
+                "messages":[
+                    {
+                        "role": "system",
+                        "content": "You are a helpful assistant."
+                    },
+                    {
+                        "role": "user",
+                        "content": "你是谁？"
+                    }
+                ]
+            },
+            "parameters": {
+                "result_format": "message"
+            }
+        }
+        """;
+
+        HttpResponse response = HttpRequest.post(url)
+                .header("Authorization", "Bearer " + apiKey)
+                .header("Content-Type", "application/json")
+                .body(jsonBody)
+                .timeout(20000)
+                .execute();
+
+        String result = response.body();
+        System.out.println(result);
+    }
+}
